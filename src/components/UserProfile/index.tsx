98844e8949c193ef58265a8c8deb8155fdb0d450
Michael Tesar <michtesar@gmail.com>
2023-02-24 22:44:42 +0100

    Add user chip

--- src/components/UserProfile/index.tsx
+++ src/components/UserProfile/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import {CircularProgress} from "@mui/material";
+import {Avatar, Chip, CircularProgress} from "@mui/material";
 
 export function UserProfile(props: { username: string | undefined, avatar_url: string | undefined }) {
-    const dimension = 80
-
     return (
         <div>
             {props.username && props.avatar_url && (
-                <div>
-                    <img src={props.avatar_url} alt={props.username} width={dimension} height={dimension}/>
-                    <p>{props.username}</p>
-                </div>
+                <Chip
+                    avatar={<Avatar alt={props.username} src={props.avatar_url}/>}
+                    label={props.username}
+                    variant="outlined"
+                />
             )}
             {(!props.username || !props.avatar_url) && <CircularProgress/>}
         </div>
