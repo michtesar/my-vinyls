dd9e8f9f51bf7f5698b892daabe7a72b3145aa69
Michael Tesar <michtesar@gmail.com>
2023-02-24 22:51:56 +0100

    Add themes

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,25 @@ import {fetchUser} from "./api/FetchUser";
 import {UserProfile} from "./components/UserProfile";
 import {CollectionView} from "./components/CollectionView/CollectionView";
 import {ViewModeSelect} from "./components/CollectionView/ViewModeSelect";
+import {createTheme, CssBaseline, ThemeProvider, useMediaQuery} from "@mui/material";
 
 
 function App() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
     const [releases, setReleases] = useState<Release[]>([])
     const [user, setUser] = useState<User | undefined>(undefined)
     const [viewMode, setViewMode] = useState("thumb")
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
 
     useEffect(() => {
         fetchUser()
@@ -32,13 +45,12 @@ function App() {
     }, [user])
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <ViewModeSelect viewMode={viewMode} setViewMode={setViewMode}/>
-                <UserProfile username={user?.username} avatar_url={user?.avatar_url}/>
-                <CollectionView releases={releases} viewMode={viewMode}/>
-            </header>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <ViewModeSelect viewMode={viewMode} setViewMode={setViewMode}/>
+            <UserProfile username={user?.username} avatar_url={user?.avatar_url}/>
+            <CollectionView releases={releases} viewMode={viewMode}/>
+        </ThemeProvider>
     );
 }
 
