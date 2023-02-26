723d126b6024737cd049f550d38b8d7b042adc67
Michael Tesar <michtesar@gmail.com>
2023-02-26 17:50:37 +0100

    Add a link to the users collection

--- src/components/UserProfile/user.tsx
+++ src/components/UserProfile/user.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import {Avatar, Chip, CircularProgress} from "@mui/material";
 
+function generateUserUrl(username: string): string {
+    return `https://www.discogs.com/user/${username}/collection`
+}
+
+function openUserOnDiscogs(username: string) {
+    const url = generateUserUrl(username)
+    window.open(url, '_blank');
+}
+
 export function UserProfile(props: { username: string | undefined, avatar_url: string | undefined }) {
     return (
         <div>
@@ -9,6 +18,11 @@ export function UserProfile(props: { username: string | undefined, avatar_url: s
                     avatar={<Avatar alt={props.username} src={props.avatar_url}/>}
                     label={props.username}
                     variant="outlined"
+                    onClick={() => {
+                        if (props.username) {
+                            openUserOnDiscogs(props.username)
+                        }
+                    }}
                 />
             )}
             {(!props.username || !props.avatar_url) && <CircularProgress/>}
